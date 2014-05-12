2fc9e95df3ffbd894e2fe2f3f666e22058ce2c9e
john <jonmarinelli@hotmail.com>
2014-05-12 17:00:47 -0400

    added steps 4 & 5; getting exponent & mult. inv.

--- rsa-encryption/rsa.c
+++ rsa-encryption/rsa.c
@@ -1,6 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "Array.h"
+
+#define bool int;
+#define true 1;
+#define false 0;
+
+/*
+*  Euclid's algorithm
+*/
+int greatestCommonDivisor(int a, int b)
+{
+	int remainder = a % b;
+	int quotient = a / b;
+
+	if(remainder == 0){
+		return b;
+	}
+	else{
+		return greatestCommonDivisor(b, remainder);
+	}
+}
 
 void primeFactorization(int n, int div, Array* array)
 {
@@ -23,6 +44,35 @@ void primeFactorization(int n, int div, Array* array)
 }
 
 /*
+*  Extended Euclidean algorithm
+*/
+int getMultiplicativeInv(int a, int n)
+{
+	int t = 0;
+	int newt = 1;
+	int r = n;
+	int newr = a;
+
+	while(newr != 0){
+		int quotient = r / newr;
+		t = newt;
+		newt = t - quotient * newt;
+
+		r = newr;
+		newr = r - quotient * newr;
+	}
+
+	if(r > 1){
+		return -1;
+	}
+	if(t < 0){
+		t = t + n;
+	}
+
+	return t;
+}
+
+/*
 *  euler's totient function: computes # of ints less than n that are coprime
 *  to n
 */
@@ -39,8 +89,30 @@ float totient(int n, Array* array)
 	return totient;
 }
 
+int getRandomCoprime(int min, int max, Array* primeFactors)
+{
+	if(min > max){
+		return -1;
+	}
+
+	//trivial coprime
+	//int e = max - 1;
+	int e = rand() % (max - min) + min;
+	int gcd = greatestCommonDivisor(e, max);
+	
+	if(gcd != 1){
+		return getRandomCoprime(min, max, primeFactors);
+	}
+	else{
+		printf("random coprime: %d\n", e);
+		return e;
+	}
+}
+
 int main(int argc, char* args[])
 {
+	srand(time(NULL));
+
 	int p = 17;
 	int q = 13;
 	int n = p * q;
@@ -55,7 +127,14 @@ int main(int argc, char* args[])
 	primeFactorization(n, 2, &array);
 	displayArray(&array);
 
-	printf("totient: %g\n", totient(n, &array));
+	int tnt = totient(n, &array);
+
+	printf("totient: %d\n", tnt);
+	
+	int e = getRandomCoprime(1, tnt, &array);
+
+	int d = getMultiplicativeInv(e, tnt);
+	printf("multinv: %d\n", d);
 
 	return 0;
 }
